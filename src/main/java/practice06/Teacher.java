bc362a42bc1b192df51ae619cfb707c205eb0509
Laughinggg <953651397@qq.com>
2022-07-20 17:12:15 +0800

    feat: implement pratice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,33 @@
 package practice06;
 
-public class Teacher {
+import practice05.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    @Override
+    public String introduce(){
+        return this.klass==null?
+                super.introduce()+" I am a Teacher. I teach No Class.":
+                String.format("%s I am a Teacher. I teach Class %d.",super.introduce(),this.klass.getNumber());
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student stu){
+        return this.klass.getNumber()==stu.getKlass().getNumber()?
+                String.format("%s I am a Teacher. I teach %s.",super.introduce(),stu.getName())
+                :String.format("%s I am a Teacher. I don't teach %s.",super.introduce(),stu.getName());
+
+    }
 }
