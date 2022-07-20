238d673d3a53c2138b983f028b23137328873c12
Laughinggg <953651397@qq.com>
2022-07-20 21:28:12 +0800

    feat: implement pratice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,43 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    protected String name;
+    protected int age;
+    protected int id;
+    public Person(int id,String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Person)) return false;
+        Person person = (Person) object;
+        return id==person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,33 @@
 package practice08;
 
-public class Student {
+
+public class Student extends  Person{
+
+    protected Klass klass;
+    public Student(int id, String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass=klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce(){
+        return super.introduce() +
+                " I am a Student."+
+                (isLeader()?
+                        String.format(" I am Leader of Class %d.",getKlass().getNumber()):
+                        String.format(" I am at Class %d.",getKlass().getNumber())
+                );
+    }
+
+    public boolean isLeader(){
+        if(this.klass.getLeader()==null) return false;
+        return this.klass.getLeader().getId()==this.id;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
