826c50f7fad6ccec0bef0ec3d1054aede8f2642a
Laughinggg <953651397@qq.com>
2022-07-20 17:01:21 +0800

    feat: implement pratice05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person{
+    private int klass=0;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass=klass;
+    }
+
+    @Override
+    public String introduce(){
+        return this.klass==0?
+                super.introduce()+" I am a Teacher. I teach No Class.":
+                String.format("%s I am a Teacher. I teach Class %d.",super.introduce(),this.klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
