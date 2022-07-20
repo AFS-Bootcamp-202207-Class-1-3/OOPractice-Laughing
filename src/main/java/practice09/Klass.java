0990e00b13589274bf5ba5ec2240ef968a3ef991
Laughinggg <953651397@qq.com>
2022-07-20 22:01:47 +0800

    feat: implement pratice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,36 @@
 package practice09;
 
+
 public class Klass {
+    private int number;
+    private Student leader;
+    public Klass(int number) {
+        this.number = number;
+    }
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public boolean assignLeader(Student stu){
+        if(stu.getKlass().getNumber()!=this.number){
+            System.out.print("It is not one of us.\n");
+            return false;
+        }
+        this.leader=stu;
+        return true;
+    }
+
+    public void appendMember(Student stu){
+        stu.setKlass(this);
+    }
 }
