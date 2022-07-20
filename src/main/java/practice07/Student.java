97f437d4e9b03b56f38c06824e41199355a8c1b0
Laughinggg <953651397@qq.com>
2022-07-20 21:10:10 +0800

    feat: implement pratice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+
+    protected Klass klass;
+    public Student(int id,String name, int age,Klass klass) {
+        super(id,name, age);
+        this.klass=klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce(){
+        return super.introduce() +
+                " I am a Student."+
+                (isLeader()?
+                 String.format(" I am Leader of Class %d.",getKlass().getNumber()):
+                 String.format(" I am at Class %d.",getKlass().getNumber())
+                );
+    }
+
+    public boolean isLeader(){
+        if(this.klass.getLeader()==null) return false;
+        return this.klass.getLeader().getId()==this.id;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,31 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id,String name, int age) {
+        super(id,name, age);
+    }
+    public Teacher(int id,String name, int age, Klass klass) {
+        super(id,name, age);
+        this.klass=klass;
+    }
+
+    @Override
+    public String introduce(){
+        return this.klass==null?
+                super.introduce()+" I am a Teacher. I teach No Class.":
+                String.format("%s I am a Teacher. I teach Class %d.",super.introduce(),this.klass.getNumber());
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student stu){
+        return this.klass.getNumber()==stu.getKlass().getNumber()?
+                String.format("%s I am a Teacher. I teach %s.",super.introduce(),stu.getName())
+                :String.format("%s I am a Teacher. I don't teach %s.",super.introduce(),stu.getName());
+
+    }
 }
